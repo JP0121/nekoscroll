9ebf703591818c767029e0499c72765b3b0c0eda
Jacob P. <thatcellphonerepairguy@gmail.com>
2026-04-14 01:45:16 -0400

    Testing the database

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const sqlite3 = require('sqlite3').verbose(); // Bringing the database back
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Try to initialize the database
+const db = new sqlite3.Database('./gallery.db', (err) => {
+    if (err) {
+        console.error("Database Error:", err.message);
+    } else {
+        console.log("Connected to the SQLite database.");
+    }
+});
+
 app.get('/', (req, res) => {
-    res.send('<h1>Server is alive! The 503 is gone!</h1>');
+    res.send('<h1>Server is alive AND the Database is working!</h1>');
 });
 
 app.listen(PORT, () => {
